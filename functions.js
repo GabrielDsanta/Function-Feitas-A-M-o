e83245b343405b5290134a0fa4141687b779aa3d
GabrielDSanta <eloviveiros@hotmail.com>
2022-09-13 21:21:01 -0300

    Funções feitas

---
+++ functions.js
@@ -0,0 +1,83 @@
+
+
+let myArray = [8, 2, 4, 5, 3, 6, 7]
+
+//pop
+
+// console.log(Remover())
+
+// function Remover(){
+//     return myArray.length = (myArray.length - 1)
+// }
+
+
+
+//push
+
+// console.log(AdicionarNoFinal())
+
+// function AdicionarNoFinal(){
+//     return myArray[myArray.length] = 9
+// }
+
+
+
+// indexof
+
+// let Valor = Number(prompt("Qual Valor Você gostária de saber a posição ?"))
+
+// console.log(PosicaoIndex())
+
+// function PosicaoIndex(){
+//     for(let i = 0; i < myArray.length; i++){
+//         if(myArray[i] == Valor){
+//             return i
+//         }
+//     }
+// }
+
+
+
+
+// includes
+
+// let Valor2 = Number(prompt("Qual Número Você gostária de saber se existe no Array ?"))
+
+// console.log(Inclusos())
+
+// function Inclusos(){
+//     let index
+
+//     for(index = 0; index < myArray.length; index++){
+//         if(myArray[index] == Valor2){
+//             return true
+//         }
+//     }
+
+//     if(index == myArray.length){
+//         return false
+//     }
+// }
+
+
+
+
+// sort
+
+// Ordernar()
+
+// console.log(myArray)
+
+// function Ordernar(){
+//     let MaiorValor = 0
+
+//     for(let contador = 0; contador < myArray.length; contador++){
+//         for(let contador2 = 0; contador2 < contador; contador2++){
+//             if(myArray[contador2] > myArray[contador2 + 1]){
+//                 MaiorValor = myArray[contador2]
+//                 myArray[contador2] = myArray[contador2 + 1]
+//                 myArray[contador2 + 1] = MaiorValor
+//             }
+//         }
+//     }
+// }
